08173b65132ad4534d6c004e2bc5b100c92be63e
Tracy Backes <tbackes@twitter.com>
2021-03-05 10:52:26 -0700

    initial community visualization

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const dscc = require('@google/dscc');
+const viz = require('@google/dscc-scripts/viz/initialViz.js');
+const local = require('./localMessage.js');
+
+// write viz code here
+const drawViz = (data) => {
+  viz.readmeViz();
+  viz.firstViz(data);
+};
+
+// renders locally
+if (DSCC_IS_LOCAL) {
+  drawViz(local.message);
+} else {
+  dscc.subscribeToData(drawViz, {transform: dscc.objectTransform});
+}
